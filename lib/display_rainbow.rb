afc312632cc1bf6f0002d12c225be88498e7e4cf
Diogo Costa <dsgmcosta@gmail.com>
2019-02-10 12:48:25 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,12 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  count = 0
+  result = ""
+  colors.length.times do
+    result = result + "#{colors[count][0].upcase}: #{colors[count]}, "
+    count = count.next
+  end
+  result.pop
+  result.pop
+  puts ""
+end
